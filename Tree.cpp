8350982503084b7aa8442032ec5b5e7017619896
paras nayal <parasnayal955@gmail.com>
2022-12-14 22:21:43 +0530

    tree data structure

---
+++ Tree.cpp
@@ -0,0 +1,30 @@
+
+// A tree is a non linear data structure because it does not stores element in a sequential manner . It stores element in a hierarchial manner as elements are stores in multiple level.
+
+// Root => Root node is a node from which the entire tree originates. It does not have a parent.
+// Parent Node => An immediate predecessor of any node is its parent node.
+// Child Node => All immediate successors of a node are its children.
+// Leaf => Node which does not have any child is a leaf.
+// Edge => Edge is the connection represented by a line between one node to another.In a tree with n nodes, there will be ‘n-1’ edges in a tree.
+// Siblings => nodes with common parents are considered to be siblings.
+// Path => Path is a number of successive edges from source node to destination node.
+// Height of Node => Height of a node represents the number of edges on the longest path between that node and a leaf.
+// Levels of node => Level of a node represents the generation of a node.
+// Degree of Node => Degree of a node implies the number of child nodes a node has.
+// Internal Node => A node that has at least one child is known as an internal node.
+// Traversing => Traversing is a process of visiting each node in a specific order in a tree data structure.
+// Ancestor node => An ancestor or ancestors to a node are all the predecessor nodes from root until that node.
+// Descendant => Immediate successor of a node is its descendent.
+// Depth of node => : Depth of a specific node x is defined as the length from root till this x node.. One edge contributes to one unit in the length.
+// depth = L + 1
+// Height of the Tree: The height of a tree is the length of the longest path from the root of the tree to a leaf node of the tree.
+
+// a recursive data structure is the one that contains itself. A tree can be viewed as a recursive data structure, as even though a tree has only one root node, each node acts as a root node to another sub-tre.
+
+// Basic Operation Of Tree:
+// Create – create a tree in data structure.
+// Insert − Inserts data in a tree.
+// Search − Searches specific data  in a tree to check it is present or not.
+// Preorder Traversal – perform Traveling a tree in a pre-order manner in data structure .
+// In order Traversal – perform Traveling a tree in an in-order manner.
+// Post order Traversal –perform Traveling a tree in a post-order manner.
